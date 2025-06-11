2ab95fea8e99da53fa0a73209bbc39775d967189
blctm <blinares@cetim.es>
2025-06-11 11:30:46 +0200

    Update IO.py

--- utils/IO.py
+++ utils/IO.py
@@ -2,5 +2,6 @@ import pandas as pd
 import os
 
 def extract_excel_to_dataframe(uploaded_file):
-    dataframe = pd.read_excel(uploaded_file)
+    # Usa la segunda fila (índice 1) como cabecera real
+    dataframe = pd.read_excel(uploaded_file, header=1)
     return dataframe
